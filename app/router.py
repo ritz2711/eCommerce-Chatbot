2fbe7265002ad684e900ccb9ad40958c23123956
ritz2711 <sagar.ritik@gmail.com>
2026-04-19 16:06:46 +0530

    added router for chat bot

---
+++ app/router.py
@@ -0,0 +1,42 @@
+from routelayer import RouteLayer
+from semantic_router import Route, SemanticRouter
+from semantic_router.encoders import HuggingFaceEncoder
+
+encoder = HuggingFaceEncoder(
+    name='sentence-transformers/all-MiniLM-L6-v2'
+)
+
+
+faq = Route(
+    name='faq',
+    utterances=[
+        "What is the return policy of the products?",
+        "Do I get discount with the HDFC credit card?",
+        "How can I track my order?",
+        "What payment methods are accepted?",
+        "How long does it take to process a refund?",
+        "What is your return policy?",
+        "What if product is defective?",
+        "Can I return a damaged product?",
+        "Refund policy for defective items",
+        "What happens if I receive a faulty product?",
+    ],
+)
+
+sql = Route(
+    name='sql',
+    utterances=[
+        "I want to buy nike shoes that have 50% discount.",
+        "Are there any shoes under Rs. 3000?",
+        "Do you have formal shoes in size 9?",
+        "Are there any Puma shoes on sale?",
+        "What is the price of puma running shoes?",
+    ],
+)
+
+routes = [faq, sql]
+router = SemanticRouter(encoder=encoder, routes=routes, auto_sync="local")
+
+if __name__ == "__main__":
+    print(router("What is your policy on defective product?").name)
+    print(router("Pink Puma shoes in price range 1000 to 5000").name)
